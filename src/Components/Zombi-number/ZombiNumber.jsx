e09effc50dbd1f82a0d20a7c007a8282ca51f7ed
junayed <junayedmollick536@gmail.com>
2024-11-11 16:09:38 +0530

    added new sections

---
+++ src/Components/Zombi-number/ZombiNumber.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ZombiNumber.css';
+
+const ZombiNumber = () => {
+  return (
+    <>
+      
+    <section className="ZombiNumber">
+        <div className="ZombiNumber-container">
+            <div className="ZombiNumber-content-box">
+                
+            </div>
+        </div>
+    </section>
+
+    </>
+  )
+}
+
+export default ZombiNumber
